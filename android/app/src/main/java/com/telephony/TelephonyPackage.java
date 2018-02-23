058554dd3a2ba6a22403f93ea7e0d29acfadc9db
Gustavo Garcia <gustavo@lntb-mul040633.network.ctbc>
2018-02-23 11:47:05 -0300

    removing javascript package react > 0.48

--- android/app/src/main/java/com/telephony/TelephonyPackage.java
+++ android/app/src/main/java/com/telephony/TelephonyPackage.java
@@ -19,11 +19,6 @@ import java.util.List;
 public class TelephonyPackage implements ReactPackage {
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
